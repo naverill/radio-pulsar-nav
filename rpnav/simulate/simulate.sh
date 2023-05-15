812e3d81788c6c77ece903628a79682887c19a91
Nina <averill.nina@gmail.com>
2023-05-15 11:07:38 +1000

    Removed plugin files

--- rpnav/simulate/simulate.sh
+++ rpnav/simulate/simulate.sh
@@ -1,4 +1,5 @@
 export TEMPO2=~/workspace/rpnav/rpnav/simulate/T2runtime
 ptaSimulate sim.input
 tcsh msfd_sim/scripts/runScripts_master
-tempo2 -output general2 -s "{sat} {pre} {err} rslt\n" -f msfd_sim/output/real_0/J0437-4715.par  msfd_sim/output/real_0/J0437-4715.tim | grep rslt | awk '{print $1,$2,$3}' > msfd_sim/output/real_0/results.txt
+outpath=msfd_sim/output/real_0
+tempo2 -output general2 -s "{sat} {pre} {err} rslt\n" -f ${outpath}/J0437-4715.par  ${outpath}/J0437-4715.tim | grep rslt | awk '{print $1,$2,$3}' > ${outpath}/results.txt
