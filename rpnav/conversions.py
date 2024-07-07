62404e0d21b2997a8ebf0ef9d5103410418a6617
Nina Averill <ninaaverill@Ninas-MacBook-Pro.local>
2024-07-07 17:59:06 +0100

    Added docstring to pulsar file

--- rpnav/conversions.py
+++ rpnav/conversions.py
@@ -1,19 +1,27 @@
 from rpnav.constants import SPEED_OF_LIGHT
 
 
-def frequency_to_wavelength(freq: float):
+def frequency_to_wavelength(freq: float) -> float:
     """
-    Parameters
-    ----------
+    Convert frequency to wavelength 
+
+    Args:
         freq (float, Hz):
+
+    Returns:
+        Wavelength (m)
     """
     return SPEED_OF_LIGHT / freq
 
 
-def wavelength_to_frequency(wav: float):
+def wavelength_to_frequency(wav: float) -> float:
     """
-    Parameters
-    ----------
-    wav (float, m)
+    Convert wavelength to frequency 
+    
+    Args:
+        wavelength (m)
+
+    Returns:
+        Frequency (Hz)
     """
     return SPEED_OF_LIGHT / wav
